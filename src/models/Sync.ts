26d8063b27e418c2ca6ad61b014a73e4269cf1a3
JustinJeong <justin.jeong5@gmail.com>
2021-10-10 00:55:59 +0900

    tsconfig

---
+++ src/models/Sync.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { HasId } from '../interfaces/hasId';
+
+export class Sync<T extends HasId> {
+	constructor(public rootUrl: string) {}
+
+	fetch(id: number): Promise<T> {
+		return axios.get(`${this.rootUrl}/${id}`);
+	}
+	save(data: T): Promise<T> {
+		const { id } = data;
+		if (id) {
+			return axios.put(`${this.rootUrl}/${id}`, data);
+		} else {
+			return axios.post(`${this.rootUrl}`, data);
+		}
+	}
+}
